2dec7851e9e09af5463190362b1a8e30598cda4e
Michael Klishevich <busation@gmail.com>
2018-05-06 00:35:11 +0300

    rotating when swipe

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // eslint-disable-next-line
-import { View, Animated, PanResponder } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class Ball extends Component {
   static propTypes = {
@@ -17,10 +24,10 @@ export default class Ball extends Component {
 
   constructor(props) {
     super(props);
-    this.position = new Animated.ValueXY();
-    console.log(this.position);
-    console.log('x', this.position.x._value);
-    console.log('y', this.position.y._value);
+    const position = new Animated.ValueXY();
+    // console.log(this.position);
+    // console.log('x', this.position.x._value);
+    // console.log('y', this.position.y._value);
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => {
         console.log('PanResponder started');
@@ -30,32 +37,56 @@ export default class Ball extends Component {
         console.log(gesture);
         console.log('dx', gesture.dx);
         console.log('dy', gesture.dy);
-        const x = this.position.x._value + gesture.dx;
-        const y = this.position.y._value + gesture.dy;
+        // const x = this.position.x._value + gesture.dx;
+        // const y = this.position.y._value + gesture.dy;
         // Animated.spring(this.position, { toValue: { x, y } }).start();
         // this.position.setValue({ x, y });
-        this.position.setValue({ x: gesture.dx, y: gesture.dy });
-        console.log('x', this.position.x._value);
-        console.log('y', this.position.y._value);
+        position.setValue({ x: gesture.dx });
+        console.log('x', position.x._value);
+        console.log('y', position.y._value);
       },
       onPanResponderRelease: () => {
         console.log('PanResponder released');
       }
     });
+    this.state = { position };
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
   }
 
   renderCards() {
-    return this.props.data.map(item => this.props.renderCard(item));
+    console.log(222);
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            { ...this.panResponder.panHandlers }
+            style={ this.getCardStyle() }
+            key={ item.id }
+          >
+            { this.props.renderCard(item) }
+          </Animated.View>
+        );
+      }
+      return  this.props.renderCard(item);
+    })
   }
 
   render() {
     return (
-      <Animated.View
-        { ...this.panResponder.panHandlers }
-        style={ this.position.getLayout() }
-      >
+      <View>
         { this.renderCards() }
-      </Animated.View>
+      </View>
     );
   }
 }
